71c86b57e2b82035bbd745835027bf62dc789829
Andrew Minion <andy@Andrews-MacBook-Pro.local>
2011-12-30 16:26:05 -0500

    WWNTBM - add custom missionaries post type

--- functions.php
+++ functions.php
@@ -1,5 +1,35 @@
 <?php
 
+// Add missionaries custom post type
+function create_post_type() {
+	register_post_type( 'wwntbm_missionaries',
+		array(
+			'labels' => array(
+				'name' => __( 'Missionaries' ),
+				'singular_name' => __( 'Missionary' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'missionaries')
+		)
+	);
+}
 
+// Styling for the custom post type icon
+function wwntbm_custom_post_icons() {
+	?>
+	<style type="text/css" media="screen">
+		#menu-posts-wwntbm_missionaries .wp-menu-image {
+			background: url(<?php echo get_stylesheet_directory_uri(); ?>/images/user.png) no-repeat 6px -16px !important;
+		}
+		#menu-posts-wwntbm_missionaries:hover .wp-menu-image, #menu-posts-wwntbm_missionaries.wp-has-current-submenu .wp-menu-image {
+			background-position:6px 8px !important;
+		}
+	</style>
+<?php }
+
+
+add_action( 'init', 'create_post_type' );
+add_action( 'admin_head', 'wwntbm_custom_post_icons' );
 
 ?>
