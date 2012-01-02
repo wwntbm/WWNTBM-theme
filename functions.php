073ae92e6ee91f490180098ca93ec99f87558ab2
Andrew Minion <andy@Andrews-MacBook-Pro.local>
2012-01-02 17:49:48 -0500

    WWNTBM - create custom user roles & add custom post type capabilities to admin

--- functions.php
+++ functions.php
@@ -42,7 +42,56 @@ function wwntbm_custom_post_icons() {
 	</style>
 <?php }
 
+// Add missionaries custom role
+$author_role = get_role('author');
+ 
+	// get_role returns an object; we want the capabilities piece, which is an array.
+	$missionary_caps = $author_role->capabilities;
+	 
+	// Remove the stuff we don't want in the new role.
+	unset($missionary_caps['edit_missionary_info']);
+	unset($missionary_caps['edit_missionary_infos']);
+	unset($missionary_caps['edit_others_missionary_infos']);
+	unset($missionary_caps['publish_missionary_infos']);
+	unset($missionary_caps['read_missionary_infos']);
+	unset($missionary_caps['delete_missionary_infos']);
+	 
+	// Add the new role.
+	add_role('missionary', 'Missionary', $missionary_caps);
 
+// Add custom post capabilities
+$administrator_role = get_role('administrator');
+	$administrator_role->add_cap( 'edit_others_missionary_infos' );
+	$administrator_role->add_cap( 'edit_missionary_info' );
+	$administrator_role->add_cap( 'edit_missionary_infos' );
+	$administrator_role->add_cap( 'edit_others_missionary_infos' );
+	$administrator_role->add_cap( 'publish_missionary_infos' );
+	$administrator_role->add_cap( 'read_missionary_infos' );
+	$administrator_role->add_cap( 'delete_missionary_infos' );
+
+
+// Add office staff custom role 
+	// get_role returns an object; we want the capabilities piece, which is an array.
+	$office_staff_caps = $administrator_role->capabilities;
+	 
+	// set custom capabilities
+	unset($office_staff_caps['install_plugins']);
+	unset($office_staff_caps['activate_plugins']);
+	unset($office_staff_caps['delete_plugins']);
+	unset($office_staff_caps['edit_plugins']);
+	unset($office_staff_caps['install_themes']);
+	unset($office_staff_caps['delete_themes']);
+	unset($office_staff_caps['edit_themes']);
+	unset($office_staff_caps['switch_themes']);
+	unset($office_staff_caps['promote_users']);
+	 
+	// Add the new role.
+	add_role('office_staff', 'Office Staff', $office_staff_caps);
+
+
+
+
+// create custom post type
 add_action( 'init', 'create_post_type' );
 add_action( 'admin_head', 'wwntbm_custom_post_icons' );
 
