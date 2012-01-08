ef58fa85a88dc69a1136cda67bd9525f49ff423c
Andrew Minion <andy@Andrews-MacBook-Pro.local>
2012-01-07 20:41:42 -0500

    WWNTBM - Countries archive page - sort by country, use dropdown lists to show missionaries by country

--- page-countries.php
+++ page-countries.php
@@ -16,24 +16,33 @@ get_header(); ?>
 					<h1 class="entry-title">Countries</h1>
 				</header><!-- .entry-header -->
 					<div class="entry-content">
+					<ul class="dropdown">
 					<?php
 					
-					// get missionaries by field
-					$query = new WP_Query( array ( 'post_type' => 'wwntbm_missionaries', 'orderby' => 'meta_value', 'meta_key' => 'Field', 'order' => 'ASC' ) );
+					// get unique countries
+					$fields = $wpdb->get_col($wpdb->prepare("SELECT DISTINCT meta_value FROM $wpdb->postmeta WHERE meta_key = 'Field' ORDER BY meta_value ASC", $metakey) );
 					
-					while ( $query->have_posts() ) : $query->the_post();
-	
-						echo '<h2><a href="';
-						the_permalink();
-						echo '">';
-						the_title();
-						echo '</a>';
-						$wwntbm_field = get_post_meta(get_the_ID(), 'Field', true);
-						if ($wwntbm_field != NULL) {echo '<span class="field-of-service"> &mdash; '.$wwntbm_field.'</span>';}
-						echo '</h2>';
-	
-					endwhile; // end of the loop. ?>
-					</div>
+					foreach ($fields as $field) {
+						// get missionaries by field
+						$query = new WP_Query( array ( 'post_type' => 'wwntbm_missionaries', 'orderby' => 'meta_value', 'meta_key' => 'Field', 'meta_value' => $field, 'order' => 'ASC' ) );
+						
+						echo '<li><a class="dropdown_trigger"><span class="trigger_pointer_arrow"></span>'.$field.'</a>
+						<ul class="sub_links" style="display:none;">';
+						while ( $query->have_posts() ) : $query->the_post();
+		
+							echo '<li><a href="';
+							the_permalink();
+							echo '">';
+							the_title();
+							echo '</a></li>';
+		
+						endwhile; // end of the loop.
+						echo '</ul>
+						</li>';
+					} // end of foreach
+					?>
+					</ul><!-- .dropdown -->
+					</div><!-- .entry-content -->
 				</article><!-- .page -->
 
 			</div><!-- #content -->
