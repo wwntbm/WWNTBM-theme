8083d08fc5322e17f543e98cc0783ecf6bd76d8e
Andrew Minion <andy@Andrews-MacBook-Pro.local>
2012-01-07 20:27:56 -0500

    WWNTBM - create Countries archive page

---
+++ page-countries.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+		<div id="primary">
+			<div id="content" role="main">
+				<article id="post-<?php the_ID(); ?>" class="post-<?php the_ID(); ?> page type-page status-publish hentry">
+				<header class="entry-header">
+					<h1 class="entry-title">Countries</h1>
+				</header><!-- .entry-header -->
+					<div class="entry-content">
+					<?php
+					
+					// get missionaries by field
+					$query = new WP_Query( array ( 'post_type' => 'wwntbm_missionaries', 'orderby' => 'meta_value', 'meta_key' => 'Field', 'order' => 'ASC' ) );
+					
+					while ( $query->have_posts() ) : $query->the_post();
+	
+						echo '<h2><a href="';
+						the_permalink();
+						echo '">';
+						the_title();
+						echo '</a>';
+						$wwntbm_field = get_post_meta(get_the_ID(), 'Field', true);
+						if ($wwntbm_field != NULL) {echo '<span class="field-of-service"> &mdash; '.$wwntbm_field.'</span>';}
+						echo '</h2>';
+	
+					endwhile; // end of the loop. ?>
+					</div>
+				</article><!-- .page -->
+
+			</div><!-- #content -->
+		</div><!-- #primary -->
+
+<?php get_footer(); ?>
