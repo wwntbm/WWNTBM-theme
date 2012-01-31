3772a1d7dedc2c23e1ac798ad992ec8e66a55fb5
Andrew Minion <andy@Andrews-MacBook-Pro.local>
2012-01-30 19:16:06 -0500

    WWNTBM - add Google+ <link rel="publisher">

--- header.php
+++ header.php
@@ -46,6 +46,7 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
+<link href="https://plus.google.com/115475849181267677638" rel="publisher" />
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <!--[if lt IE 9]>
