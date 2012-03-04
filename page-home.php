d11dbe82b4965b8fb93c0ca95e381fb86ed88c3a
Andrew Minion <andrew@minionsformissions.com>
2012-03-03 20:28:35 -0500

    WWNTBM - add responsive buttons to home page

--- page-home.php
+++ page-home.php
@@ -18,33 +18,29 @@ get_header(); ?>
 		<div id="primary">
 			<div id="content" role="main">
 
-			<?php if ( have_posts() ) : ?>
-
 				<?php twentyeleven_content_nav( 'nav-above' ); ?>
-
-				<?php /* Start the Loop */ ?>
-				<?php while ( have_posts() ) : the_post(); ?>
-
-					<?php get_template_part( 'content', get_post_format() ); ?>
-
-				<?php endwhile; ?>
+				
+				<a class="home-button" id="who" href="<?php bloginfo('url'); ?>/connect/missionaries/" title="Who We Are">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/home-buttons/who-we-are.jpg" alt="Who We Are" />
+					<h2>Who We Are</h2>
+				</a>
+				
+				<a class="home-button" id="where" href="<?php bloginfo('url'); ?>/connect/countries/" title="Where We Serve">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/home-buttons/where-we-serve.png" alt="Where We Serve" />
+					<h2>Where We Serve</h2>
+				</a>
+				
+				<a class="home-button" id="beliefs" href="<?php bloginfo('url'); ?>/learn/our-doctrinal-statement/" title="What We Believe">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/home-buttons/what-we-believe.jpg" alt="What We Believe" />
+					<h2>What We Believe</h2>
+				</a>
+				
+				<a class="home-button" id="contact" href="<?php bloginfo('url'); ?>/connect/contact-wwntbm/" title="Who We Are">
+					<img src="<?php bloginfo('stylesheet_directory'); ?>/images/home-buttons/how-to-contact.jpg" alt="How to Contact Us" />
+					<h2>How to Contact Us</h2>
+				</a>
 
 				<?php twentyeleven_content_nav( 'nav-below' ); ?>
-
-			<?php else : ?>
-
-				<article id="post-0" class="post no-results not-found">
-					<header class="entry-header">
-						<h1 class="entry-title"><?php _e( 'Nothing Found', 'twentyeleven' ); ?></h1>
-					</header><!-- .entry-header -->
-
-					<div class="entry-content">
-						<p><?php _e( 'Apologies, but no results were found for the requested archive. Perhaps searching will help find a related post.', 'twentyeleven' ); ?></p>
-						<?php get_search_form(); ?>
-					</div><!-- .entry-content -->
-				</article><!-- #post-0 -->
-
-			<?php endif; ?>
 
 			</div><!-- #content -->
 		</div><!-- #primary -->
