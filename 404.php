39043816ca7df3db40397cf955897bd062e72268
Andrew Minion <andrew@minionsformissions.com>
2012-05-09 09:27:47 -0400

    WWNTBM - add 404 page

---
+++ 404.php
@@ -0,0 +1,35 @@
+<?php
+/**
+ * The template for displaying 404 pages (Not Found).
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary">
+		<div id="content" role="main">
+
+			<article id="post-0" class="post error404 not-found">
+				<header class="entry-header">
+					<h1 class="entry-title">We&rsquo;re Sorry!</h1>
+				</header>
+
+				<div class="entry-content">
+					<p>It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching or one of the links below can help.</p>
+
+					<?php get_search_form(); ?>
+
+					<?php the_widget( 'WP_Widget_Recent_Posts', array( 'number' => 10 ), array( 'widget_id' => '404-posts' ) ); ?>
+
+					<?php the_widget( 'WP_Widget_Pages','number=10' ); ?>
+
+				</div><!-- .entry-content -->
+			</article><!-- #post-0 -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php get_footer(); ?>
