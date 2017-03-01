4abc02fa290cfa72d9d55441a58b7c272f19fb47
Andrew Minion <andrew@minionsformissions.com>
2017-03-01 16:34:47 -0500

    remove PayPal donation buttons from each missionary page

--- content-missionary.php
+++ content-missionary.php
@@ -73,20 +73,6 @@
 		}
 		?>
 
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_donations">
-            <input type="hidden" name="business" value="HL2ZG8QDMLLE2">
-            <input type="hidden" name="lc" value="US">
-            <input type="hidden" name="item_name" value="World Wide New Testament Baptist Missions">
-            <input type="hidden" name="item_number" value="<?php the_title(); ?>">
-            <input type="hidden" name="no_note" value="1">
-            <input type="hidden" name="no_shipping" value="1">
-            <input type="hidden" name="currency_code" value="USD">
-            <input type="hidden" name="bn" value="PP-DonationsBF:btn_donateCC_LG.gif:NonHosted">
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
-            <img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1">
-        </form>
-
 		<?php
 		// prayer letters
 		// get missionary unique key
