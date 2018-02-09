b6a9c6f41d5f9b74326e4d1e7815b5537a8401ad
Andrew Minion <andrew@minionsformissions.com>
2018-02-09 14:21:02 -0500

    add visual total cost indicator

--- js/tithely-faith-promise-cards.js
+++ js/tithely-faith-promise-cards.js
@@ -15,12 +15,15 @@
             printingCost = cardQuantityChoice.data('printing'),
             shippingPriorityCost = cardQuantityChoice.data('priority-cost'),
             shippingExpressCost = cardQuantityChoice.data('express-cost'),
-            selectedOptions = ' - ' + cardStyle + ' style - ' + cardQuantity + ' quantity - ' + shippingMethod + ' shipping';
+            shippingCost = cardQuantityChoice.data(shippingMethodOption.val()+'-cost'),
+            selectedOptions = ' - ' + cardStyle + ' style - ' + cardQuantity + ' quantity - ' + shippingMethod + ' shipping',
+            totalAmount = Number(Number(printingCost) + Number(shippingCost)).toFixed(2);
 
         $('.fp-donation-input.priority .cost').html('$'+shippingPriorityCost);
         $('.fp-donation-input.express .cost').html('$'+shippingExpressCost);
 
-        $('.tithely-give-btn').data('giving-to', 'Faith Promise Cards'+selectedOptions);
-console.log($('.tithely-give-btn').data('giving-to'));
+        var buttonText = 'Donate $'+totalAmount+' to cover printing/shipping costs';
+
+        $('.tithely-give-btn').text(buttonText).attr('data-giving-to', 'Faith Promise Cards'+selectedOptions).attr('data-amount', totalAmount);
     }
 })(jQuery);
