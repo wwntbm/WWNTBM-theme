219e50a3c92caf961e9fe6afd55ebdb811ae8619
Andrew Minion <andrew@andrewrminion.com>
2019-11-23 12:16:57 -0600

    fix code style

--- functions.php
+++ functions.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Theme functions.
+ *
+ * @package wwntbm
+ */
 
 define( 'WWNTBM_THEME_VERSION', wp_get_theme()->get( 'Version' ) );
 
@@ -6,10 +11,10 @@ define( 'WWNTBM_THEME_VERSION', wp_get_theme()->get( 'Version' ) );
  * Enqueue Google fonts and add cache-busting query string to stylesheet
  */
 function wwntbm_assets() {
-	wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css?family=Lato:400,400i,900,900i' );
+	wp_enqueue_style( 'fonts', 'https://fonts.googleapis.com/css?family=Lato:400,400i,900,900i' ); // phpcs:ignore WordPress.WP.EnqueuedResourceParameters.MissingVersion
 	wp_enqueue_style( 'stylesheet', get_stylesheet_uri(), array(), WWNTBM_THEME_VERSION );
 
-	wp_enqueue_script( 'frontend', get_stylesheet_directory_uri() . '/js/frontend.min.js', array( 'jquery' ), WWNTBM_THEME_VERSION );
+	wp_enqueue_script( 'frontend', get_stylesheet_directory_uri() . '/js/frontend.min.js', array( 'jquery' ), WWNTBM_THEME_VERSION, true );
 }
 add_action( 'wp_enqueue_scripts', 'wwntbm_assets' );
 
@@ -47,7 +52,7 @@ function wwntbm_user_roles() {
 	$office_staff_role->add_cap( 'read_private_pages' );
 	$office_staff_role->add_cap( 'manage_options' );
 
-	// Add missionaries custom role
+	// Add missionaries custom role.
 	$author_role = get_role( 'author' );
 
 	// get_role returns an object; we want the capabilities piece, which is an array.
@@ -65,7 +70,7 @@ function wwntbm_user_roles() {
 	// Add the new role.
 	add_role( 'missionary', 'Missionary', $missionary_caps );
 
-	// add custom capabilities
+	// add custom capabilities.
 	$missionary_role = get_role( 'missionary' );
 	$missionary_role->add_cap( 'edit_missionary_update' );
 	$missionary_role->add_cap( 'edit_missionary_updates' );
@@ -77,8 +82,8 @@ add_action( 'init', 'wwntbm_user_roles' );
 /**
  * Display missionary prayer letter names.
  *
- * @param string $file
- * @param string $missionary_name_key
+ * @param string $file                File.
+ * @param string $missionary_name_key Missionary name.
  *
  * @return void
  * @since 1.0.0
@@ -88,42 +93,48 @@ function missionary_prayer_letters( $file, $missionary_name_key ) {
 	$month_numbers = array( '01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12' );
 	$month_names = array( 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December' );
 
-	// rename prayer letter
+	// rename prayer letter.
 	$prayer_letter_title = str_ireplace( $missionary_name_key . '---', '', ( basename( $file, '.' . $file_extension ) ) );
 
-	// filter out month
+	// filter out month.
 	$prayer_letter_month_array = explode( '-', $prayer_letter_title );
 	$prayer_letter_month = $prayer_letter_month_array[1];
 	$prayer_letter_year = $prayer_letter_month_array[0];
 
-	// build title
+	// build title.
 	$prayer_letter_title = str_replace( $month_numbers, $month_names, $prayer_letter_month ) . ' ' . $prayer_letter_year;
 
-	// check for special titles
-	if ( $prayer_letter_month_array[2] != null ) {
+	// check for special titles.
+	if ( ! is_null( $prayer_letter_month_array[2] ) ) {
 		$prayer_letter_title .= ' (' . $prayer_letter_month_array[2] . ')';
 	}
 
-	// echo the <li> item
-	echo '<li><a target="_blank" href="' . esc_url( get_site_url( null, '/' . $file ) ) . '" title="' . $prayer_letter_title . '">' . $prayer_letter_title . '</a></li>' . "\n";
+	// echo the <li> item.
+	echo '<li><a target="_blank" href="' . esc_url( get_site_url( null, '/' . $file ) ) . '" title="' . esc_attr( $prayer_letter_title ) . '">' . wp_kses_post( $prayer_letter_title ) . '</a></li>' . "\n";
 }
-// end missionary prayer letter names
-// add sidebar for home page
-function WWNTBM_widgets_init() {
+
+/**
+ * Add sidebar for home page.
+ *
+ * @return void
+ */
+function wwntbm_widgets_init() {
 	register_sidebar(
 		array(
-			'name' => __( 'Homepage Sidebar', 'twentyeleven' ),
-			'id' => 'sidebar-6',
+			'name'          => __( 'Homepage Sidebar', 'wwntbm' ),
+			'id'            => 'sidebar-6',
 			'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-			'after_widget' => '</aside>',
-			'before_title' => '<h3 class="widget-title">',
-			'after_title' => '</h3>',
+			'after_widget'  => '</aside>',
+			'before_title'  => '<h3 class="widget-title">',
+			'after_title'   => '</h3>',
 		)
 	);
 }
+add_action( 'widgets_init', 'wwntbm_widgets_init' );
 
-
-// add custom thumbnail size for missionary archive page
+/**
+ * Custom thumbnail sizes.
+ */
 if ( function_exists( 'add_theme_support' ) ) {
 	add_theme_support( 'post-thumbnails' );
 	add_image_size( 'category-thumb', 150, 100, true );
@@ -134,14 +145,23 @@ if ( function_exists( 'add_theme_support' ) ) {
 	add_image_size( 'missionary_of_the_day_l', 500, 300, true );
 }
 
-
-// add sidebar to all pages
+/**
+ * Add sidebar to all pages.
+ *
+ * @return void
+ * @since 1.0.0
+ */
 function my_child_theme_setup() {
-	// Removes the filter that adds the "singular" class to the body element which centers the content and does not allow for a sidebar
+	// Removes the filter that adds the "singular" class to the body element which centers the content and does not allow for a sidebar.
 	remove_filter( 'body_class', 'twentyeleven_body_classes' );
 }
-// end add sidebar to all pages
-// hide widgets
+
+/**
+ * Hide widgets.
+ *
+ * @return void
+ * @since 1.0.0
+ */
 function remove_dashboard_widgets() {
 	remove_meta_box( 'dashboard_quick_press', 'dashboard', 'side' );
 	remove_meta_box( 'dashboard_incoming_links', 'dashboard', 'normal' );
@@ -151,59 +171,95 @@ function remove_dashboard_widgets() {
 	remove_meta_box( 'dashboard_primary', 'dashboard', 'side' );
 	remove_meta_box( 'dashboard_secondary', 'dashboard', 'side' );
 }
-// end hide widgets
-// AndrewRMinion branding
-	// change login page link
-	function wpc_url_login() {
-		return 'https://wwntbm.com/';
-	}
-	add_filter( 'login_headerurl', 'wpc_url_login' );
 
-	// customize login page logo
-	function login_css() {
-		wp_enqueue_style( 'login_css', get_stylesheet_directory_uri() . '/login.css' );
-	}
-	add_action( 'login_head', 'login_css' );
+/**
+ * Change login page link.
+ *
+ * @return string Login page URL.
+ * @since 1.0.0
+ */
+function wpc_url_login() {
+	return 'https://wwntbm.com/';
+}
+add_filter( 'login_headerurl', 'wpc_url_login' );
 
-	// custom admin footer
-	function remove_footer_admin() {
-		echo 'Developed by <a href="https://andrewrminion.com/" target="_blank">AndrewRMinion Design</a>.';
-	}
-	add_filter( 'admin_footer_text', 'remove_footer_admin' );
+/**
+ * Customize login logo.
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function login_css() {
+	wp_enqueue_style( 'login_css', get_stylesheet_directory_uri() . '/login.css', array(), WWNTBM_THEME_VERSION );
+}
+add_action( 'login_head', 'login_css' );
 
-	// technical info widget
-	function armd_dashboard_widget_function() {
-	// Entering the text between the quotes
-		echo '<ul>
-        <li>Release Date: May 2012</li>
-        <li>Developer: <a href="https://andrewrminion.com/" target="_blank">AndrewRMinion Design</a></li>
-        <li>Hosting provider: ANHosting (WWNTBM account)</li>
-        </ul>';
-	}
-	function armd_add_dashboard_widgets() {
-		wp_add_dashboard_widget( 'wp_dashboard_widget', 'Technical information', 'armd_dashboard_widget_function' );
-	}
-	add_action( 'wp_dashboard_setup', 'armd_add_dashboard_widgets' );
-// end AndrewRMinion branding
-// add actions
-add_action( 'widgets_init', 'WWNTBM_widgets_init' );
+/**
+ * Custom admin footer.
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function remove_footer_admin() {
+	echo 'Developed by <a href="https://andrewrminion.com/" target="_blank">AndrewRMinion Design</a>.';
+}
+add_filter( 'admin_footer_text', 'remove_footer_admin' );
+
+/**
+ * Technical info widget.
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function armd_dashboard_widget_function() {
+	echo '<ul>
+	<li>Release Date: May 2012</li>
+	<li>Developer: <a href="https://andrewrminion.com/" target="_blank">AndrewRMinion Design</a></li>
+	<li>Hosting provider: ANHosting (WWNTBM account)</li>
+	</ul>';
+}
+
+/**
+ * Add dashboard widgets.
+ *
+ * @return void
+ * @since 1.0.0
+ */
+function armd_add_dashboard_widgets() {
+	wp_add_dashboard_widget( 'wp_dashboard_widget', 'Technical information', 'armd_dashboard_widget_function' );
+}
+add_action( 'wp_dashboard_setup', 'armd_add_dashboard_widgets' );
+
 add_action( 'after_setup_theme', 'my_child_theme_setup' );
 
 if ( ! current_user_can( 'manage_options' ) ) {
 	add_action( 'wp_dashboard_setup', 'remove_dashboard_widgets' );
 
-	// hide some admin menu options
+	/**
+	 * Hide some admin menu items.
+	 *
+	 * @return void
+	 * @since 1.0.0
+	 */
 	function my_remove_menu_pages() {
-		remove_menu_page( 'upload.php' ); // Media
-		remove_menu_page( 'edit-comments.php' ); // Comments
-		remove_menu_page( 'edit.php?post_type=slide' ); // Slides
+		remove_menu_page( 'upload.php' ); // Media.
+		remove_menu_page( 'edit-comments.php' ); // Comments.
+		remove_menu_page( 'edit.php?post_type=slide' ); // Slides.
 	}
 	add_action( 'admin_menu', 'my_remove_menu_pages' );
-	// hide Contact Form
+
+	// hide Contact Form.
 	define( 'WPCF7_ADMIN_READ_CAPABILITY', 'manage_options' );
 	define( 'WPCF7_ADMIN_READ_WRITE_CAPABILITY', 'manage_options' );
 
-	// hide other author's posts
+	/**
+	 * Hide posts from other authors.
+	 *
+	 * @param WP_Query $query Query object.
+	 *
+	 * @return WP_Query Query object.
+	 * @since 1.0.0
+	 */
 	function posts_for_current_author( $query ) {
 		global $pagenow;
 
