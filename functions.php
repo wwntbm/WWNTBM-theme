8387d2cce9d702c6c8e3560dfbfdf760d09b419d
Andrew Minion <andrew@andrewrminion.com>
2020-04-11 19:01:59 -0500

    add video templates

--- functions.php
+++ functions.php
@@ -310,3 +310,20 @@ if ( function_exists( 'acf_add_options_page' ) ) {
 		)
 	);
 }
+
+/**
+ * Print HTML with meta information for the current post-date/time and author.
+ *
+ * Create your own twentyeleven_posted_on to override in a child theme
+ *
+ * @since Twenty Eleven 1.0
+ */
+function twentyeleven_posted_on() {
+	printf(
+		__( '<span class="sep">Posted on </span><a href="%1$s" title="%2$s" rel="bookmark"><time class="entry-date" datetime="%3$s">%4$s</time></a>', 'twentyeleven' ),
+		esc_url( get_permalink() ),
+		esc_attr( get_the_time() ),
+		esc_attr( get_the_date( 'c' ) ),
+		esc_html( get_the_date() )
+	);
+}
